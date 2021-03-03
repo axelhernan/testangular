ef5eb9c2facb31ccee3b162ef1e979127181aaf7
axelhernan <axeel.hernan@gmail.com>
2021-03-03 11:50:11 -0300

    fix loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,40 @@
 import { ProductosService } from './services/productos.service';
 import { InfoPaginaService } from './services/info-pagina.service';
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import {
+  Router,
+  NavigationStart,
+  NavigationCancel,
+  NavigationEnd,
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  loading = true;
 
-  constructor(public infoPagina: InfoPaginaService, public infoProducts: ProductosService) {
+  constructor(
+    public infoPagina: InfoPaginaService,
+    public infoProducts: ProductosService,
+    public router: Router
+  ) {}
 
+  ngAfterViewInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        setTimeout(() => {
+          this.loading = true;
+        }, 1000);
+      } else if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel
+      ) {
+        this.loading = false;
+      }
+    });
   }
 
   title = 'portafolio';
